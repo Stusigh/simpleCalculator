c636e5b5bdde58c1d1f7b6bcbaf4158ed11566c4
Stusigh <stuartjohnpage@live.com>
2021-02-02 10:48:52 -0600

    added square method

--- src/calculatorPackage/Calculator.java
+++ src/calculatorPackage/Calculator.java
@@ -10,7 +10,17 @@ public class Calculator {
         return num1 - num2;
     }
     //Multiplication Method
-    public Integermultiply(int num1, int num2) {
-
+    public Integer multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    //Division Method
+    public Integer divide(int num1, int num2) {
+        return num1/num2;
+    }
+    //Square Method
+    public Integer square(int num1) {
+        return num1*num1;
     }
 }
+
+
