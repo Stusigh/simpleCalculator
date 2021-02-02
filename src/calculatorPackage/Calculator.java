a51b2c481b5a026c178bf55091a42ef799b811d7
Stusigh <stuartjohnpage@live.com>
2021-02-02 10:45:46 -0600

    inital commit

---
+++ src/calculatorPackage/Calculator.java
@@ -0,0 +1,16 @@
+package calculatorPackage;
+
+public class Calculator {
+    //Addition Method
+    public Integer add(int num1, int num2) {
+        return num1 + num2;
+    }
+    //Subtraction Method
+    public Integer subtract( int num1, int num2) {
+        return num1 - num2;
+    }
+    //Multiplication Method
+    public Integermultiply(int num1, int num2) {
+
+    }
+}
---
+++ src/calculatorPackage/Main.java
@@ -0,0 +1,8 @@
+package calculatorPackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
