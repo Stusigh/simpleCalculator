17b63a6a0a452bac1598e9842517a7e2b505fd1d
Stusigh <stuartjohnpage@live.com>
2021-02-03 08:44:24 -0600

    added 3 decimal places to squareroot calculator.

--- src/calculatorPackage/MagicCalculator.java
+++ src/calculatorPackage/MagicCalculator.java
@@ -1,27 +1,29 @@
 package calculatorPackage;
 
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class MagicCalculator extends Calculator {
     //Finding the Square Root of a Number
-    public static double squareRootOf(int inputNumber) {
-        int answer = (int)Math.sqrt(inputNumber);
-        return answer;
+    public static String squareRootOf(int inputNumber) {
+        float answer = (float)Math.sqrt(inputNumber);
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setRoundingMode(RoundingMode.CEILING);
+        String answerToReturn = df.format(answer);
+        return answerToReturn;
     }
     //Finding the Sine of a number:
     public static double sinOfNumber(int inputNumber) {
-        float answer = (float) Math.sin(inputNumber);
-        return answer;
+        return (float) Math.sin(inputNumber);
     }
     //Finding the Cosine of a number:
     public static double coSinOfNumber(int inputNumber) {
-        float answer = (float) Math.cos(inputNumber);
-
-        return answer;
+        return (float) Math.cos(inputNumber);
     }
     //Finding the Tangent of a number:
     public static double tanOfNumber(int inputNumber) {
-        float answer = (float) Math.tan(inputNumber);
-        return answer;
+        return (float) Math.tan(inputNumber);
     }
     //Finding the Factorial of a number:
     public static int factorialOfNumber(int inputNumber) {
