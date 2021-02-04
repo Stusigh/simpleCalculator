b562598f96e8c195eaf3cee3d495a7a017623b09
Stusigh <stuartjohnpage@live.com>
2021-02-04 10:07:29 -0600

    fixed string of add, fixed factorial to handle 0 and -1, changed floats to doubles.

--- src/calculatorPackage/MagicCalculator.java
+++ src/calculatorPackage/MagicCalculator.java
@@ -6,36 +6,42 @@ import java.text.DecimalFormat;
 public class MagicCalculator extends Calculator {
     //Finding the Square Root of a Number
     public static double squareRootOf(int inputNumber) {
-        return (float) Math.sqrt(inputNumber);
+        return Math.sqrt(inputNumber);
     }
 
     //Finding the Sine of a number:
     public static double sinOfNumber(int inputNumber) {
-        return (float) Math.sin(inputNumber);
+        return Math.sin(inputNumber);
     }
 
     //Finding the Cosine of a number:
     public static double coSinOfNumber(int inputNumber) {
-        return (float) Math.cos(inputNumber);
+        return Math.cos(inputNumber);
     }
 
     //Finding the Tangent of a number:
     public static double tanOfNumber(int inputNumber) {
-        return (float) Math.tan(inputNumber);
+        return  Math.tan(inputNumber);
     }
 
     //Finding the Factorial of a number:
     public static int factorialOfNumber(int inputNumber) {
-        int answer = inputNumber;
-        for(int i=inputNumber-1; i>0; --i) {
-            answer = answer * i;
+        if(inputNumber == 0) {
+            return 1;
+        } else if(inputNumber < 0) {
+            return 0;
+        } else {
+            int answer = inputNumber;
+            for(int i=inputNumber-1; i>0; --i) {
+                answer = answer * i;
+            }
+            return answer;
         }
-        return answer;
     }
 
     //Rounding an answer to n decimal places:
     //This makes a lot of garbage but I couldn't get StringBuffer to work
-    public static String roundThisAnswer(float inputNumber, int spacesDesired) {
+    public static String roundThisAnswer(double inputNumber, int spacesDesired) {
         String spacesToAdd = "#.";
         for( int i=0; i<spacesDesired; i++) {
             spacesToAdd = spacesToAdd + "#";
--- src/calculatorPackage/Main.java
+++ src/calculatorPackage/Main.java
@@ -4,29 +4,29 @@ public class Main {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 2;
-        int num3 = 5;
+        int num3 = 10;
         int decimalPlacesToRoundTo = 3;
         //I can use MagicCalculator or Calculator because MagicCalculator extends Calculator! Which is pretty cool.
-        System.out.println(num1 + " added to " + num2 +" equals: " + MagicCalculator.add(10,2));
+        System.out.println(num1 + " added to " + num2 +" equals: " + MagicCalculator.add(num1,num2));
 
-        System.out.println(num1 + " subtracted from " + num2 +" equals: " + Calculator.subtract(10,2));
+        System.out.println(num2 + " subtracted from " + num1 +" equals: " + Calculator.subtract(num1,num2));
 
-        System.out.println(num1 + " multiplied by " + num2 +" equals: " + Calculator.multiply(10,2));
+        System.out.println(num1 + " multiplied by " + num2 +" equals: " + Calculator.multiply(num1,num2));
 
-        System.out.println(num1 + " divided by " + num2 +" equals: " + Calculator.divide(10,2));
+        System.out.println(num1 + " divided by " + num2 +" equals: " + Calculator.divide(num1,num2));
 
-        System.out.println(num1 + " squared equals: " + Calculator.square(10));
+        System.out.println(num1 + " squared equals: " + Calculator.square(num1));
 
-        System.out.println("The square root of " + num3 + " is: " + MagicCalculator.roundThisAnswer((float) MagicCalculator.squareRootOf(num3),
+        System.out.println("The square root of " + num3 + " is: " + MagicCalculator.roundThisAnswer(MagicCalculator.squareRootOf(num3),
                 decimalPlacesToRoundTo) + ", rounded to " + decimalPlacesToRoundTo + " decimal places.");
 
-        System.out.println("The sine of " + num3 + " is: " + MagicCalculator.roundThisAnswer((float) MagicCalculator.sinOfNumber(num3),
+        System.out.println("The sine of " + num3 + " is: " + MagicCalculator.roundThisAnswer(MagicCalculator.sinOfNumber(num3),
                 decimalPlacesToRoundTo) + ", rounded to " + decimalPlacesToRoundTo + " decimal places.");
 
-        System.out.println("The cosine of " + num3 + " is: " + MagicCalculator.roundThisAnswer((float) MagicCalculator.coSinOfNumber(num3),
+        System.out.println("The cosine of " + num3 + " is: " + MagicCalculator.roundThisAnswer(MagicCalculator.coSinOfNumber(num3),
                 decimalPlacesToRoundTo) + ", rounded to " + decimalPlacesToRoundTo + " decimal places.");
 
-        System.out.println("The tangent of " + num3 + " is: " + MagicCalculator.roundThisAnswer((float) MagicCalculator.tanOfNumber(num3),
+        System.out.println("The tangent of " + num3 + " is: " + MagicCalculator.roundThisAnswer(MagicCalculator.tanOfNumber(num3),
                 decimalPlacesToRoundTo) + ", rounded to " + decimalPlacesToRoundTo + " decimal places.");
 
         System.out.println("The factorial of " + num3 + " is: " + MagicCalculator.factorialOfNumber(num3) + ".");
